0c22b91fbea99f95624e2a61de740320acd0c0de
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-07-28 19:40:49 +0900

    add CVIW cpp file

--- CVIW.cpp
+++ CVIW.cpp
@@ -3,32 +3,28 @@
 #include <time.h>
 #include <iostream>
 #include <windows.h>
+#include <math.h>
+#include <fstream>
+#include <string>
 
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+using namespace std;
 
-using namespace cv;
+// 0: right up = m(y-1) + x + 1
+// 1: right = my + x + 1
+// 2: right down = m(y+1) + x + 1
+// 3: down = m(y+1) + x
+// 4: left down = m(y+1) + x - 1
+// 5: left = my + x - 1
+// 6: left up = m(y-1) + x - 1
+// 7: up = m(y-1) + x
 
-// 0: 오른쪽 위 = m(y-1) + x + 1
-// 1: 오른쪽 = my + x + 1
-// 2: 오른쪽 아래 = m(y+1) + x + 1
-// 3: 아래 = m(y+1) + x
-// 4: 왼쪽 아래 = m(y+1) + x - 1
-// 5: 왼쪽 = my + x - 1
-// 6: 왼쪽 위 = m(y-1) + x - 1
-// 7: 위 = m(y-1) + x
-
-// weight[좌표][위의 방향 중 하나]
-
-
-// 추후에는 힙을 사용하여 SIZE 를 늘린다. 
-#define SIZE 25000
+// weight[index][0 ~ 7]
 
 
+#define SIZE 17000
 
 // ---------------- start -------------------- CVIW ---------------- start ------------------------
-// 하나의 CVIW 클래스 
+// CVIW class
 class CVIW {
 public:
 	int m;
@@ -47,29 +43,29 @@ public:
 };
 
 
-// 첫번째 케이스 가중치 
+// first case Weight
 void CVIW::case1Weight() {
 	int x, y;
 	double w_;
 
 	for (x = 0; x <= m - 2; x++) {
 		for (y = 1; y <= n - 2; y++) {
-			// 오른쪽 위 
+			// right up
 			w_ = 1.0 * P[m * (y - 1) + x + 1] / P[m * y + x];
 			weight[m * y + x][0] = w_;
 			weight[m * (y - 1) + x + 1][4] = 1 / w_;
 
-			// 오른쪽 
+			// right
 			w_ = 1.0 * P[m * y + x + 1] / P[m * y + x];
 			weight[m * y + x][1] = w_;
 			weight[m * y + x + 1][5] = 1 / w_;
 
-			// 오른쪽 아래 
+			// right down
 			w_ = 1.0 * P[m * (y + 1) + x + 1] / P[m * y + x];
 			weight[m * y + x][2] = w_;
 			weight[m * (y + 1) + x + 1][6] = 1 / w_;
 
-			// 아래 
+			// down
 			w_ = 1.0 * P[m * (y + 1) + x] / P[m * y + x];
 			weight[m * y + x][3] = w_;
 			weight[m * (y + 1) + x][7] = 1 / w_;
@@ -78,22 +74,22 @@ void CVIW::case1Weight() {
 }
 
 
-// 두번째 케이스 가중치		
+// second case Weight	
 void CVIW::case2Weight() {
 	double w_;
 
 	for (int x = 0; x <= m - 2; x++) {
-		// 오른쪽 
+		// right
 		w_ = 1.0 * P[x + 1] / P[x];
 		weight[x][1] = w_;
 		weight[x + 1][5] = 1 / w_;
 
-		// 오른쪽 아래
+		// right down
 		w_ = 1.0 * P[x + m + 1] / P[x];
 		weight[x][2] = w_;
 		weight[x + m + 1][6] = 1 / w_;
 
-		// 아래
+		// down
 		w_ = 1.0 * P[x + m] / P[x];
 		weight[x][3] = w_;
 		weight[x + m][7] = 1 / w_;
@@ -101,18 +97,18 @@ void CVIW::case2Weight() {
 }
 
 
-// 세번째 케이스 가중치		
+// third case Weight
 void CVIW::case3Weight() {
 	int x, y = n - 1;
 	double w_;
 
 	for (x = 0; x <= m - 2; x++) {
-		// 오른쪽 위
+		// right up
 		w_ = 1.0 * P[m * (y - 1) + x + 1] / P[m * y + x];
 		weight[m * y + x][0] = w_;
 		weight[m * (y - 1) + x + 1][4] = 1 / w_;
 
-		// 오른쪽
+		// right
 		w_ = 1.0 * P[m * y + x + 1] / P[m * y + x];
 		weight[m * y + x][1] = w_;
 		weight[m * y + x + 1][5] = 1 / w_;
@@ -120,13 +116,13 @@ void CVIW::case3Weight() {
 }
 
 
-// 네번째 케이스 가중치
+// fourth case Weight
 void CVIW::case4Weight() {
 	int x = m - 1, y;
 	double w_;
 
 	for (y = 0; y <= n - 2; y++) {
-		// 아래 
+		// down
 		w_ = 1.0 * P[m * (y + 1) + x] / P[m * y + x];
 		weight[m * y + x][3] = w_;
 		weight[m * (y + 1) + x][7] = 1 / w_;
@@ -134,7 +130,7 @@ void CVIW::case4Weight() {
 }
 
 
-// 가중치 출력 함수 
+// print Weight 
 void CVIW::printWeight() {
 	for (int i = 0; i < m * n; i++) {
 		for (int j = 0; j < 8; j++) {
@@ -146,7 +142,7 @@ void CVIW::printWeight() {
 }
 
 
-// 가중치 구하는 함수 
+// Weight
 void CVIW::Weight_() {
 	if (m == 0 or n == 0) {
 		printf("m and n not must be 0. defined it again please.");
@@ -160,11 +156,11 @@ void CVIW::Weight_() {
 }
 
 
-// CVIW 를 Init 하는 메소드 
+// CVIW Init Members
 void CVIW::InitMembers(int m_, int n_, int P_[]) {
 	m = m_;
 	n = n_;
-	for (int i = 0; i < m * n; i++) P[i] = P_[i];
+	for (int i = 0; i < m * n; i++) P[i] = P_[i] + 1;
 }
 // ----------- ----- end ------------------- CVIW ------------------- end -----------------------
 
@@ -173,7 +169,7 @@ void CVIW::InitMembers(int m_, int n_, int P_[]) {
 
 // ----------- start --------------------- CVIW_GROUP --------------------- start ---------------------
 
-// 여러 CVIW 를 한번에 관리하는 클래스. 
+// CVIW GROUP CLASS
 class CVIW_GROUP {
 public:
 	CVIW cviws[1001];
@@ -186,7 +182,7 @@ public:
 	void Group();
 };
 
-// CVIW 를 추가하는 메소드. 
+// add CVIW
 void CVIW_GROUP::add_cviw(int P[]) {
 	if (m == 0 or n == 0) {
 		printf("m and n not must be 0. defined it again please.");
@@ -202,7 +198,7 @@ void CVIW_GROUP::add_cviw(int P[]) {
 	idx++;
 }
 
-// CVIW_GROUP 에 있는 모든 CVIW의 가중치를 한번에 구하는 메소드. 
+// all the CVIW in CVIW_GROUP are weight extract.
 void CVIW_GROUP::WeightAll() {
 	if (size < 1) {
 		printf("size not must be smaller than 1. defined it again please.");
@@ -215,41 +211,78 @@ void CVIW_GROUP::WeightAll() {
 
 
 
+double cost(CVIW cviw1, CVIW cviw2, int m, int n) {
+	double c = 0.0;
+
+	for (int i = 0; i < m * n; i++) {
+		for (int j = 0; j < 8; j++) {
+			c += abs(cviw1.weight[i][j] - cviw2.weight[i][j]);
+		}
+	}
+
+	return c;
+}
+
 
 // ------------ start ---------------------- MAIN --------------------- start --------------------
 int main() {
 	srand(time(NULL));
 
-	CVIW cviw;
-	cviw.m = 156; cviw.n = 156;
 
-	for (int i = 0; i < 156 * 156; i++) cviw.P[i] = rand() % 256 + 1;    // P[i] 가 0 인 경우에는 가중치가 무조건 0 이 나오므로 1 을 더해준다. 
+	CVIW cviw1, cviw2;
+
+	cviw1.m = 128; cviw1.n = 128;
+	cviw2.m = 128; cviw2.n = 128;
+
+	ifstream fin;
+
+	char line[200];
+
+	fin.open("C:\\kimdonghwan\\python\\CVIW\\male2.txt");
+	if (fin.is_open()) {
+		int i = 0;
+		while (fin.getline(line, sizeof(line))) {
+			cviw1.P[i] = atoi(line) + 1;
+			i++;
+		}
+		cviw1.Weight_();
+		printf("\n");
+	}
+	fin.close();
+
+	//for (int i = 0; i < 128 * 128; i++) cviw1.P[i] = rand() % 256 + 1;
+	//cviw1.Weight_();
+
+
+	fin.open("C:\\kimdonghwan\\python\\CVIW\\male1.txt");
+	if (fin.is_open()) {
+		int i = 0;
+		while (fin.getline(line, sizeof(line))) {
+			cviw2.P[i] = atoi(line) + 1;
+			i++;
+		}
+		cviw2.Weight_();
+		printf("\n");
+
+	}
+	fin.close();
+
+	//for (int i = 0; i < 128 * 128; i++) cviw2.P[i] = rand() % 256 + 1;
+	//cviw2.Weight_();
+
+	double c = cost(cviw1, cviw2, 128, 128);
+	printf("%lf", c);
 
 
 
-	clock_t start = clock();
-	cviw.Weight_();
-	printf("Time: %lf\n", 1.0 * (clock() - start) / CLOCKS_PER_SEC);
+	//	clock_t start = clock();
+	//	cviw.Weight_();
+	//	printf("Time: %lf\n", 1.0 * (clock() - start) / CLOCKS_PER_SEC);
 
 
-	Sleep(1000);
+	//	Sleep(1000);
 
-	cviw.printWeight();
-
-	
-	// cv library test
-	Mat female1 = imread("C:\\Users\\SimplePro\\Downloads\\female1.jpg", 0);
-	imshow("female1", female1);
-
-	Mat female2 = imread("C:\\Users\\SimplePro\\Downloads\\female2.jpg", 0);
-	imshow("female2", female2);
-
-	Mat male1 = imread("C:\\Users\\SimplePro\\Downloads\\male1.jpg", 0);
-	imshow("male1", male1);
-
-	Mat male2 = imread("C:\\Users\\SimplePro\\Downloads\\male2.jpg", 0);
-	imshow("male2", male2);
-	waitKey(0);
+	//	cviw.printWeight();
 
 	return 0;
 }
