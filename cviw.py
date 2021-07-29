59201addfbce811e43e8e27d70abb558a8b66c60
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-07-29 22:21:23 +0900

    add cviw python file

--- cviw.py
+++ cviw.py
@@ -1,4 +1,6 @@
 import numpy as np
+import cv2
+
 
 class CVIW:
     def __init__(self, m, n, P) -> None:
@@ -13,6 +15,8 @@ class CVIW:
         self.case3Weight()
         self.case4Weight()
 
+
+    # first case weight method
     def case1Weight(self):
         m = self.m
         P = self.P
@@ -40,6 +44,7 @@ class CVIW:
                 self.weight[m * (y + 1) + x][7] = 1 / w_
 
 
+    # second case weight method
     def case2Weight(self):
         P = self.P
         m = self.m
@@ -61,6 +66,7 @@ class CVIW:
             self.weight[x + m][7] = 1 / w_
 
 
+    # third case weight method
     def case3Weight(self):
         y = self.n - 1
         m = self.m
@@ -78,6 +84,7 @@ class CVIW:
             self.weight[m * y + x + 1][5] = 1 / w_
 
 
+    # fourth case weight method
     def case4Weight(self):
         P = self.P
         m = self.m
@@ -90,15 +97,65 @@ class CVIW:
             self.weight[m * (y + 1) + x][7] = 1 / w_
 	
 
+    # print weight method
     def printWeight(self):
         for i in range(self.m * self.n):
             for j in range(8):
-                print(self.weight[i][j], end=" ")
+                print(round(self.weight[i][j], 3), end=" ")
             
             print("", end = "\t")
             if((i+1) % self.m == 0): print("", end="\n")
 
-cviw = CVIW(3, 3, np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-cviw.Weight_()
 
-cviw.printWeight()
+# cost method
+def cost(weight1, weight2):
+    return sum(sum(abs(weight1 - weight2)))  # 차이의 합
+
+
+
+# CVIW GROUP class
+class CVIW_GROUP:
+    def __init__(self, class_name = "", cviws = [], dsize = (128, 128)) -> None:
+        self.cviws: list = cviws
+        self.class_name: str = class_name
+        self.important_weight: list = []
+        self.dsize: tuple = dsize
+
+    # add cviw method
+    def add_cviw(self, cviw) -> None:
+        self.cviws.append(cviw)
+
+
+    # all cviw in cviws list get weight.
+    def weight_all(self) -> None:
+        for i in range(len(self.cviws)):
+            self.cviws[i].Weight_()
+
+    
+    # load then add cviw method.
+    def load_add_cviw(self, file):
+        img = cv2.resize(cv2.cvtColor(cv2.imread(file, 1), cv2.COLOR_BGR2GRAY), dsize=self.dsize).flatten().tolist()
+        cviw = CVIW(self.dsize[0], self.dsize[1], img)
+        cviw.Weight_()
+        self.add_cviw(cviw)
+
+
+# load image method
+def load(file, dsize_ = (128, 128)) -> list:
+    img = cv2.resize(cv2.cvtColor(cv2.imread(file, 1), cv2.COLOR_BGR2GRAY), dsize=dsize_)
+    img = img.flatten().tolist()
+
+    return img
+    
+
+
+if __name__ == '__main__':
+    cviw = CVIW(3, 3, np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    cviw.Weight_()
+
+    cviw.printWeight()
+
+    cviw2 = CVIW(3, 3, np.array([5, 2, 3, 4, 1, 3, 4, 2, 6]))
+    cviw2.Weight_()
+
+    print(f"cost: {cost(cviw.weight, cviw2.weight)}")
