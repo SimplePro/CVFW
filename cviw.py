2ffb52213b8b061f72bc0ffa8283004e9eeed7b9
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-07-31 21:29:25 +0900

    add cviw (without gpu) python file

--- cviw.py
+++ cviw.py
@@ -1,7 +1,11 @@
 import numpy as np
 import cv2
 from numpy.core.function_base import linspace
-
+from os import listdir
+import pickle
+from numba import cuda
+# import code
+# code.interact(local=locals)
 
 
 class CVIW:
@@ -9,7 +13,7 @@ class CVIW:
         self.m = m
         self.n = n
         self.weight = np.zeros([50000, 8])
-        self.P = P
+        self.P = (np.array(P) + 1).tolist()
 
     
     def Weight_(self):
@@ -86,6 +90,7 @@ class CVIW:
             self.weight[m * y + x][1] = w_
             self.weight[m * y + x + 1][5] = 1 / w_
 
+
     # fourth case weight method
     def case4Weight(self):
         P = self.P
@@ -124,7 +129,6 @@ class COST_FUNCTION:
         self.weights = np.append(self.weights, weight)
 
 
-
     # 가중치들을 그룹화하는 메소드
     def avgG(self) -> None:
         groups = [[self.weights[0]]]      # 그룹 초기화
@@ -159,7 +163,7 @@ class COST_FUNCTION:
 
             # weight이 두개의 Cost function 사이에 위치한 경우
             if end1 > start2  and weight >= alpha[i] and weight <= alpha[i+1]:
-                a = 5.645 / ((alpha[i+1] - alpha[i])/2)    # tanh(ax) 에서 x 의 계수
+                a = 5.645 / ((alpha[i+1] - alpha[i])/2 + 0.00001)    # tanh(ax) 에서 x 의 계수
                 x = (alpha[i] + alpha[i+1]) / 2    # tanh(x - alpha) 에서 alpha 역할
                 b = (np.tanh((alpha[i] - alpha[i+1])/2))**2    # tanh( a(x - alpha) ) + b 에서 b 역할
 
@@ -168,7 +172,7 @@ class COST_FUNCTION:
 
             # weight이 하나의 Cost function 에 위치한 경우
             if weight >= start1 and weight <= end1:
-                a = 5.645 / ((end1 - start1) / 2)    # tanh(ax) 에서 x 의 계수
+                a = 5.645 / ((end1 - start1) / 2 + 0.00001)    # tanh(ax) 에서 x 의 계수
 
                 return np.tanh(a * (weight - alpha[i]))**2 - 0.5  # cost
 
@@ -177,7 +181,7 @@ class COST_FUNCTION:
         end1 = self.group_range[-1][1]
 
         if weight >= start1 and weight <= end1:
-            a = 5.645 / ((end1 - start1) / 2)    # tanh(ax) 에서 x 의 계수
+            a = 5.645 / ((end1 - start1) / 2 + 0.00001)    # tanh(ax) 에서 x 의 계수
 
             return np.tanh(a * (weight - alpha[-1]))**2 - 0.5  # cost
 
@@ -201,7 +205,6 @@ class CVIW_GROUP:
     # add cviw method, img is flatten list of pixel data to add.
     def add_cviw(self, img) -> None:
         cviw = CVIW(self.dsize[0], self.dsize[1], img)  # CVIW 클래스 선언
-        cviw.Weight_()  # 가중치 구하기
         self.cviws.append(cviw)  # cviws 에 cviw 추가.
 
     # all cviw in cviws list get weight.
@@ -214,24 +217,114 @@ class CVIW_GROUP:
     def load_add_cviw(self, file):
         img = cv2.resize(cv2.cvtColor(cv2.imread(file, 1), cv2.COLOR_BGR2GRAY), dsize=self.dsize).flatten().tolist()  # 로드한 이미지를 flatten 한다.
         cviw = CVIW(self.dsize[0], self.dsize[1], img)  # CVIW 클래스 선언
-        cviw.Weight_()  # Weight 으로 가중치들을 구한다.
-        self.add_cviw(cviw)  # cviws 에 해당 cviw 를 추가한다.
+        self.cviws.append(cviw)  # cviws 에 해당 cviw 를 추가한다.
 
     
     
     # train method.
     def train_(self):
+        self.weight_all()  # 가중치를 먼저 구함
+
+        percent = self.dsize[0] * self.dsize[1]
+
         for i in range(self.dsize[0] * self.dsize[1]):
             self.important_weight.append([])
             for j in range(8):
                 weights = []
                 for cviw in self.cviws:
-                    weights.append(cviw.weight[i][j])
+                    weights.append(cviw.weight[i][2])
                 
-                cost = COST_FUNCTION(weights)  # cost function 으로 특징가중치를 처리한다.
+                cost = COST_FUNCTION(sorted(weights))  # cost function 으로 특징가중치를 처리한다.
                 cost.avgG()  # 특징가중치 구하는 메소드 호출
                 self.important_weight[i].append(cost)  # 특징가중치 리스트에 해당 cost function 클래스 대입
-                # print(i * 8 + j, end="\r")
+            
+            print(f'{i / percent * 100} %', end="\r")
+
+        del self.cviws
+
+
+    # cost 를 구하는 메소드.
+    def cost_function(self, weight):
+        cost = 0
+
+        for i in range(self.dsize[0] * self.dsize[1]):
+            for j in range(8):
+                cost += self.important_weight[i][j].cost(weight[i][j])
+
+        return cost
+
+
+
+# CVIW 모델 클래스
+class CVIW_MODEL:
+    def __init__(self, dsize = (128, 128)) -> None:
+        self.cviw_groups = []
+        self.classes = []
+        self.dsize = dsize
+
+    
+    # 디렉토리 경로로 CVIW_GROUP 을 추가하는 메소드.
+    def add_directory(self, class_name = "", path = "") -> None:
+        self.classes.append(class_name)
+        imgs = listdir(path)
+        cviw_group = CVIW_GROUP(class_name=class_name, dsize = self.dsize)
+
+        for img in imgs:
+            cviw_group.load_add_cviw(file = f'{path}\\{img}')
+
+        self.cviw_groups.append(cviw_group)
+
+
+    # 학습하는 메소드
+    def train(self) -> None:
+        for i in range(len(self.classes)):
+            self.cviw_groups[i].train_()
+            print(f"\n{self.classes[i]} Done!\n")
+
+
+    # 클래스를 예측하는 메소드
+    def predict_class(self, img):
+        cviw = CVIW(self.dsize[0], self.dsize[1], img)
+        cviw.Weight_()
+
+        cost_list = []
+
+        for cviw_group in self.cviw_groups:
+            cost_list.append(cviw_group.cost_function(cviw.weight))
+        
+        print(cost_list)
+        return self.classes[np.argmin(cost_list)]
+
+
+    # 모델을 저장하는 메소드.
+    def save_model(self, model_name = ""):
+        with open(f"{model_name}.p", 'wb') as file:
+            pickle.dump(self.classes, file)
+            pickle.dump(self.dsize, file)
+
+            important_weight = []
+            for cviw_group in self.cviw_groups:
+                important_weight.append([])
+
+                for iw in cviw_group.important_weight:
+                    important_weight[-1].append(iw)
+
+            pickle.dump(important_weight, file)
+
+
+    # 모델을 로드하는 메소드.
+    def load_model(self, model_path = ""):
+        with open(model_path, 'rb') as file:
+            self.classes = pickle.load(file)
+            self.dsize = pickle.load(file)
+            important_weight = pickle.load(file)
+
+            for i in range(len(important_weight)):
+                
+                cviw_group = CVIW_GROUP(class_name=self.classes[i], dsize = self.dsize)
+                cviw_group.important_weight = important_weight[i]
+
+                self.cviw_groups.append(cviw_group)
 
 
 
@@ -266,7 +359,6 @@ if __name__ == '__main__':
         cviw_group.add_cviw([randint(1, 256) for _ in range(128 * 128)])
 
     start_time = time()
-    cviw_group.weight_all()
     cviw_group.train_()
     print(f"time: {time() - start_time}")
 
@@ -274,7 +366,17 @@ if __name__ == '__main__':
     # Cost Function
     cost_function = COST_FUNCTION(weights = [1, 7, 9, 14, 15, 23, 27])
     cost_function.avgG()
-    x = linspace(3, 30, 1000)
+    x = linspace(0, 50, 1000)
     y = [cost_function.cost(i) for i in x]
     plt.plot(x, y)
     plt.show()
+
+    cviw_group = CVIW_GROUP(class_name="test", dsize=(3, 3))
+    cviw_group.add_cviw([1, 4, 4, 7, 3, 5, 6, 3, 2])
+    cviw_group.add_cviw([7, 4, 4, 6, 3, 5, 7, 2, 4])
+    cviw_group.add_cviw([14, 6, 2, 4, 6, 7, 6, 7, 8])
+    cviw_group.add_cviw([9, 9, 1, 2, 3, 5, 5, 3, 4])
+    cviw_group.add_cviw([15, 4, 3, 2, 6, 2, 3, 1, 2])
+    cviw_group.add_cviw([25, 8, 7, 2, 4, 2, 23, 4, 2])
+    cviw_group.add_cviw([23, 0, 3, 4, 3, 4, 3, 2, 3])
+    cviw_group.train_()
