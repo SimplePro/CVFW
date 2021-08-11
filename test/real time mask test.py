8df9f582f1073340fc5b61e76da10bf556fe3622
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-08-11 12:53:09 +0900

    add real time mask test python file

--- test/real time mask test.py
+++ test/real time mask test.py
@@ -1,6 +1,7 @@
 import cv2
 from cvfwVer2 import CVFW_MODEL
 
+
 model = CVFW_MODEL(dsize=(64, 64))
 
 model.add_directory(class_name="with mask", path="C:/kimdonghwan/python/CVFW/image/mask/train/with_mask")
@@ -8,15 +9,11 @@ model.add_directory(class_name="without mask", path="C:/kimdonghwan/python/CVFW/
 
 model.train()
 
-
-# 가중치 파일 경로
-cascade_filename = './haarcascade_frontalface_default.xml'
-# 모델 불러오기
-cascade = cv2.CascadeClassifier(cascade_filename)
-
 webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 count = 0
+
+with_mask = False
 
 roi_gray = []
 
@@ -24,29 +21,27 @@ while webcam:
     ret, img = webcam.read()
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.flip(img ,1)
 
-    faces = cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors = 3, minSize = (20, 20))
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x-50, y-50), (x + w + 50, y+h+50), (255, 255, 255), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+    color = (0, 0, 255)
+    if with_mask: color = (0, 255, 0)
+    elif not with_mask: color = (0, 0, 255)
+    cv2.rectangle(img, (170, 80), (470, 380), color, 2)
+    roi_gray = gray[80+20:380+20, 170:470]
+
 
     if count % 30 == 0:
         try:
             face = cv2.resize(roi_gray, dsize=(64, 64))
-            predict = model.modeling_predict_class(face.flatten().tolist())
-            print(model.classes[predict.index(min(predict))])
+            predict = model.predict_class(face.flatten().tolist())
+            with_mask = predict.index(min(predict)) == 0
 
         except: pass
 
     count += 1
 
 
-
     cv2.imshow('video', img)
     k = cv2.waitKey(1) & 0xff
     if k == 27: break
-
-webcam.release()
-cv2.destroyAllWindows()
