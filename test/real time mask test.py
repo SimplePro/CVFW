64237f2d9835bfe88b257ff5d5e1531bab8bcf1a
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-08-13 19:39:13 +0900

    add real time mask test python file

--- test/real time mask test.py
+++ test/real time mask test.py
@@ -1,5 +1,6 @@
 import cv2
 from cvfwVer2 import CVFW_MODEL
+import numpy as np
 
 
 model = CVFW_MODEL(dsize=(64, 64))
@@ -9,39 +10,45 @@ model.add_directory(class_name="without mask", path="C:/kimdonghwan/python/CVFW/
 
 model.train()
 
+with_mask_modeling = cv2.resize(model.modeling(class_name="with mask"), dsize=(300, 300))
+with_mask_modeling *= 255 / np.max(with_mask_modeling)
+
 webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 count = 0
 
 with_mask = False
 
-roi_gray = []
+mask_gray = []
 
 while webcam:
     ret, img = webcam.read()
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.flip(img ,1)
+    gray = cv2.flip(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 1)
+    # img = cv2.flip(img ,1)
 
 
-    color = (0, 0, 255)
-    if with_mask: color = (0, 255, 0)
-    elif not with_mask: color = (0, 0, 255)
-    cv2.rectangle(img, (170, 80), (470, 380), color, 2)
-    roi_gray = gray[80+20:380+20, 170:470]
+    color = (0, 0, 0)
+    if with_mask: color = (255, 255, 255)
+    elif not with_mask: color = (0, 0, 0)
+    cv2.rectangle(gray, (165, 75), (475, 385), color, 2)
+    mask_gray = gray[80:380, 170:470]
 
-
-    if count % 30 == 0:
+    if count % 3 == 0:
         try:
-            face = cv2.resize(roi_gray, dsize=(64, 64))
-            predict = model.predict_class(face.flatten().tolist())
+            face = cv2.resize(mask_gray, dsize=(64, 64))
+            predict = model.modeling_predict_class(face.flatten().tolist())
             with_mask = predict.index(min(predict)) == 0
+            print(with_mask)
 
         except: pass
 
     count += 1
 
+    alpha = 0.75
+    blended = (gray[80:380, 170:470] * alpha) + (with_mask_modeling * (1 - alpha)).astype(np.uint8)
+    gray[80:380, 170:470] = blended
 
-    cv2.imshow('video', img)
+    cv2.imshow('video', gray)
     k = cv2.waitKey(1) & 0xff
     if k == 27: break
